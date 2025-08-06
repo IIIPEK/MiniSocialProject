006f3f91748757123e842d9bada1d3eb1bec949d
Ivan Vorobev <ivan.vorobjov@gmail.com>
2025-08-06 22:34:52 +0300

    Password reset by email added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 
@@ -13,4 +14,16 @@ urlpatterns = [
     path('users/', views.user_list, name='user_list'),
     path('users/<str:username>/', views.public_profile, name='public_profile'),
     path('users/<str:username>/follow/', views.toggle_follow, name='toggle_follow'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='accounts/password_reset_form.html'),
+         name='password_reset'),
+    path('password_reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'),
+         name='password_reset_complete'),
+
 ]
